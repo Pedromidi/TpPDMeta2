7285a277d23ccb55a2ab3886354387b0fff64941
Pedro Miguel Dinis Sequeira <119708845+Pedromidi@users.noreply.github.com>
2024-12-04 19:06:36 +0000

    Springboot default project
    Há que começar por algum lado, se alguém tiver já um servidor springboot das aulas plz substituam por esse

---
+++ servidor/src/main/java/com/example/tppdm2/Tppdm2Application.java
@@ -0,0 +1,13 @@
+package com.example.tppdm2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Tppdm2Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Tppdm2Application.class, args);
+    }
+
+}
---
+++ servidor/src/test/java/com/example/tppdm2/Tppdm2ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.tppdm2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Tppdm2ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}

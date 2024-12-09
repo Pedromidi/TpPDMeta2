16562e17247814d8b194b4b4529c83df1ae7a52d
Pedro Miguel Dinis Sequeira <119708845+Pedromidi@users.noreply.github.com>
2024-12-09 11:32:02 +0000

    Delete servidor directory for new improved stuff

--- servidor/src/main/java/com/example/tppdm2/clienteRMI/ClienteRMI.java
+++
@@ -1,7 +0,0 @@
-package com.example.tppdm2.clienteRMI;
-
-import com.example.tppdm2.servidor.RMI.RMIServiceInterface;
-
-public class ClienteRMI  {
-
-}
--- servidor/src/main/java/com/example/tppdm2/clienteRMI/ObserverInterface.java
+++
@@ -1,8 +0,0 @@
-package com.example.tppdm2.clienteRMI;
-
-import java.rmi.Remote;
-import java.rmi.RemoteException;
-
-public interface ObserverInterface extends Remote {
-    void notification(String change) throws RemoteException;
-}

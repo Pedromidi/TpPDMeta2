f0c5bfb512226a0c41660a4f1946cf06337748a5
Pedro Miguel Dinis Sequeira <119708845+Pedromidi@users.noreply.github.com>
2024-12-14 18:43:35 +0000

    Update RMIApplication.java

--- tppd/src/main/java/pt/meta_II/tppd/servers/RMI/RMIApplication.java
+++ tppd/src/main/java/pt/meta_II/tppd/servers/RMI/RMIApplication.java
@@ -1,28 +1,33 @@
 package pt.meta_II.tppd.servers.RMI;
 
+import pt.meta_II.tppd.DbManager;
+
 import java.net.MalformedURLException;
 import java.rmi.AlreadyBoundException;
 import java.rmi.Naming;
 import java.rmi.RemoteException;
 import java.rmi.registry.LocateRegistry;
 import java.rmi.registry.Registry;
+
+import static java.lang.System.exit;
+
 public class RMIApplication {
 
     public static void main(String[] args) {
+
+        DbManager manager = new DbManager("../DataBase", "TP_DB.db");
+        DbManager db = DbManager.getInstance();
+        db.connect();
 
         try{
             LocateRegistry.createRegistry(Registry.REGISTRY_PORT);
 
             String url = "rmi://localhost/rmiservice";
 
-            RMIService service = new RMIService();
+            RMIService service = new RMIService(db);
 
             Naming.bind(url, service);
-        } catch (MalformedURLException e) {
-            e.printStackTrace();
-        } catch (AlreadyBoundException e) {
-            e.printStackTrace();
-        } catch (RemoteException e) {
+        } catch (MalformedURLException | AlreadyBoundException | RemoteException e) {
             e.printStackTrace();
         }
     }
